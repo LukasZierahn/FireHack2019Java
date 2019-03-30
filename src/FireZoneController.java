19e2de0dbefd675dbd9fb21d4b75bf74c705130a
Lukas Zierahn <lukas@kappa-mm.de>
2019-03-30 08:15:41 +0000

    finalised multi uav's for now

--- src/FireZoneController.java
+++ src/FireZoneController.java
@@ -1,12 +1,14 @@
 import afrl.cmasi.*;
 import afrl.cmasi.searchai.HazardZoneDetection;
 import afrl.cmasi.searchai.HazardZoneEstimateReport;
+import mil.afrl.amase.util.CmasiNavUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.lang.Math.abs;
 import static java.lang.Math.pow;
 
 public class FireZoneController {
@@ -185,7 +187,7 @@ public class FireZoneController {
         }
     }
 
-    public void RemoiveHazardZonePoint(int index) {
+    public void RemoveHazardZonePoint(int index) {
         estimatedHazardZone.remove(index);
         hazardZoneTimes.remove(index);
 
@@ -194,13 +196,42 @@ public class FireZoneController {
     }
 
     public void AddHazardZonePoint(Location3D point) {
-        for (int i = 0; i < estimatedHazardZone.size(); i++) {
+        for (int i = estimatedHazardZone.size() - 1; i >= 0; i--) {
             if (0.00002 > distance(estimatedHazardZone.get(i), point)) {
-                return;
+                if (20000 < main.getTime() - hazardZoneTimes.get(i)) {
+                    RemoveHazardZonePoint(i);
+                } else {
+                    return;
+                }
             }
         }
 
-        
+        if (estimatedHazardZone.size() >= 5) {
+            double heading1 = CmasiNavUtils.headingBetween(estimatedHazardZone.get(0), point);
+            double heading2 = CmasiNavUtils.headingBetween(estimatedHazardZone.get(0), estimatedHazardZone.get(1));
+
+            if (35 > abs(heading1 - heading2)) {
+                RemoveHazardZonePoint(0);
+            }
+
+            boolean changed = true;
+            while (changed) {
+                changed = false;
+                for (int i = estimatedHazardZone.size() - 1; i > 0; i--) {
+                    if (hazardZoneTimes.get(i) < hazardZoneTimes.get(i - 1)) {
+                        hazardZoneTimes.remove(i - 1);
+                        changed = true;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 1; i < 3; i++) {
+                if (1.4 * distance(point, estimatedHazardZone.get(0)) < distance(point, estimatedHazardZone.get(i))) {
+                    RemoveHazardZonePoint(0);
+                }
+            }
+        }
 
         estimatedHazardZone.add(point);
         hazardZoneTimes.add(main.getTime());
