ffaed514d85f188ea54389d4f05c8d64d31974af
Lukas Zierahn <lukas@kappa-mm.de>
2019-03-31 09:35:58 +0100

    routes

---
+++ src/RouteManager.java
@@ -0,0 +1,17 @@
+import afrl.cmasi.Location3D;
+import afrl.cmasi.Waypoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteManager {
+
+    public Location3D StartPoint;
+
+    public List<List<Waypoint>> routes = new ArrayList<>();
+
+
+    public RouteManager() {
+
+    }
+}
